0001b9dc44cd844cea2e873c34989377fb90144c
Kaysul Anik <kaysul.i.anik@utu.fi>
2020-09-10 14:45:34 +0300

    User Registration with jwt token successful

---
+++ middleware/auth.js
@@ -0,0 +1,22 @@
+const jwt = require('jsonwebtoken');
+const config = require('config');
+
+module.exports = function (req, res, next) {
+  // get token from the header
+  const token = req.header('x-auth-token');
+
+  // Check if not token
+  if (!token) {
+    return res.status(401).json({ msg: 'No token, authorization denied' });
+  }
+  // verify token
+  try {
+    const decoded = jwt.verify(token, config.get('jwtSecret'));
+    // take the request object and assign a value to the user
+
+    req.user = decoded.user; // decoded.user will have the user from the PAYLOAD (FROM THE DATABASE)
+    next();
+  } catch (err) {
+    res.statu(401).json({ msg: 'Token is not valid' });
+  }
+};
--- routes/api/auth.js
+++ routes/api/auth.js
@@ -1,10 +1,24 @@
 const express = require('express');
 const router = express.Router();
 
+// USE THE MIDDLEWARE
+const auth = require('../../middleware/auth');
+
+// Bring the USER model
+const User = require('../../models/User');
+
 // @route       GET api/auth
 // @desc        Test route
 // @access      Public
 
-router.get('/', (req, res) => res.send('Auth route'));
+router.get('/', auth, async (req, res) => {
+  try {
+    const user = await User.findById(req.user.id).select('-password'); //since we passed req.user = decoded.user in the auth.js (middleware), it has id
+    res.json(user);
+  } catch (err) {
+    console.err(err.message);
+    res.status(500).send('Server Error');
+  }
+});
 
 module.exports = router;
