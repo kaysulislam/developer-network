398f20e72e9513e3888c30fc99f6fa128a2d2f55
Kaysul Anik <kaysulislam@gmail.com>
2021-12-24 04:52:15 +0200

    added get profile by user_id

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -129,4 +129,26 @@ router.get('/', async (req, res) => {
   }
 });
 
+// @route       GET api/profile/user/:user_id
+// @desc        Get profile by user_id
+// @access      public
+router.get('/user/:user_id', async (req, res) => {
+  try {
+    const profile = await Profile.findOne({
+      user: req.params.user_id,
+    }).populate('user', ['name', 'avatar']);
+
+    // check if the profile exists for the user_id
+    if (!profile) return res.status(400).json({ msg: 'Profile not found' });
+
+    res.json(profile);
+  } catch (err) {
+    console.log(err.message);
+    if (err.kind === 'ObjectId') {
+      return res.status(400).json({ msg: 'Profile not found' });
+    }
+    res.status(500).send('Server Error');
+  }
+});
+
 module.exports = router;
