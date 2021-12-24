3f18b8f1e8444f0a3f378bb1262d766c69d2cd90
Kaysul Anik <kaysulislam@gmail.com>
2021-12-25 00:52:29 +0200

    modified delete experience route

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -233,6 +233,10 @@ router.delete('/experience/:exp_id', auth, async (req, res) => {
       .map(item => item.id)
       .indexOf(req.params.exp_id);
 
+    if (removeIndex === -1) {
+      return res.status(400).json('Experience not found');
+    }
+
     profile.experience.splice(removeIndex, 1);
 
     await profile.save();
